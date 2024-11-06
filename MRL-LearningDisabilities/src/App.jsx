be67f6629939eb68ca0bfb5831cfec9e4a1b46b2
SalC124 <saltcal124@gmail.com>
2024-11-06 10:54:44 -0500

    website yippee it was started and i shall fnish it later

--- MRL-LearningDisabilities/src/App.jsx
+++ MRL-LearningDisabilities/src/App.jsx
@@ -1,26 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <div className="aech-one">
-        <h1>spectrum snuffer</h1>
-      </div>
-      <main>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid eos
-          eligendi rerum quae nemo laudantium perferendis temporibus,
-          reprehenderit minus quidem voluptates officia voluptate unde culpa
-          ipsam? Corporis, quisquam adipisci numquam debitis vitae minus ipsam
-          dignissimos vero soluta sequi quis fugit beatae neque rem officia
-          sapiente laborum placeat nam non laudantium.
-        </p>
-      </main>
-      <footer>
-        <p>2024 | Salvatore Calafiore & Nate Schmude</p>
-      </footer>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
